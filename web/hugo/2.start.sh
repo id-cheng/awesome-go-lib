31ca5278bdf86e8afe97f5a56bfaf9f568f08bfc
jiacheng <id.jiacheng@gmail.com>
2024-09-29 11:17:03 +0800

    hugo

---
+++ web/hugo/2.start.sh
@@ -0,0 +1,6 @@
+hugo new site quickstart
+cd quickstart
+git init
+git submodule add https://github.com/theNewDynamic/gohugo-theme-ananke.git themes/ananke
+echo "theme = 'ananke'" >> hugo.toml
+hugo server
